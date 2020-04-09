10ed3b529c147147261dfd4d9f96e1cf9e095d03
Amanda Key <ariaandezrasmom@gmail.com>
2020-04-08 19:20:36 -0500

    completed activity

--- stringActivity.js
+++ stringActivity.js
@@ -1,11 +1,16 @@
 // Exercise One
 // Check to see if the string is empty.  Return true or false.
-var x = isEmptyString("");
-function isEmptyString() {
-    
+function isEmptyString(string) {
+    let result; 
+    if(string.length > 0){
+        result = false
+    }else {
+        result = true
+    }
     return result;
 }
 //Tests: Run these commands to make sure you did it right. They should all be true.
+console.log("Part 1");
 console.log(isEmptyString("") === true);
 console.log(isEmptyString("Hello!") === false);
 console.log(isEmptyString(" ") ===  false);
@@ -16,10 +21,16 @@ console.log(isEmptyString(" ") ===  false);
 // Hint: indexOf is case sensitive! So how can you search for any possible case of java? What if you made the entire text string lowercase? Then you searched through that?  Check out the Essential String Functions from the reading and see if there's a useful function there.
 function containsWordJava(text) {
     let result; 
-    // Add your code here.
+    let lowerCaseText = text.toLowerCase();
+    if(lowerCaseText.includes("java")){
+        result = true
+    } else {
+        result =false
+    }
     return result;
 }
 //Tests: Run these commands to make sure you did it right. They should all be true.
+console.log("Part 2");
 console.log(containsWordJava("Hello this is a message about coding.") === false);
 console.log(containsWordJava("Hello this is a message about JavaScript!") === true);
 console.log(containsWordJava("JAVA IS FUN :D") === true);
@@ -30,10 +41,14 @@ console.log(containsWordJava("JAVA IS FUN :D") === true);
 // Note that the length of the string should be maxLength including the ellipsis.  This means you'll have to subtract the lenght of the ellipsis fom the maxLength to know how many characters there should be.
 // Hint: Look at the "slice" method.  You can set the first argument to zero to make it start at the beginning of the string.   (Watch out that slice returns a new string! You need to assign it to something)
 function trimSentence(text, maxLength) {
-    // Add your code here.
+    const mylength = maxLength -3
+    if (text.length > mylength){
+        return text.slice(0, mylength) + "..."
+    }
     return text;
 }
 //Tests: Run these commands to make sure you did it right. They should all be true.
+console.log("Part 3");
 console.log(trimSentence("The fox jumped over the fence to get to the farm.", 16) === "The fox jumpe...");
 console.log(trimSentence("supercalifragilisticexpialidocious", 20) === "supercalifragilis...");
 console.log(trimSentence("Short", 16) === "Short");
